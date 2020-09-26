aa3153abc8085b538e3aa0045dfd578a0512c65e
Áron Vikete <aron.vikete@stud.uni-corvinus.hu>
2020-09-26 10:32:43 +0200

    Write to file

--- UserMaintenance/Form1.cs
+++ UserMaintenance/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,7 @@ namespace UserMaintenance
             listBox1.DisplayMember = "FullName";
             label1.Text = Resource_felirat.FullName;
             button1.Text = Resource_felirat.Add;
+            button2.Text = Resource_felirat.WriteFile;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -32,6 +34,26 @@ namespace UserMaintenance
                 FullName = textBox1.Text,
             };
             users.Add(u);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Comma Seperated Values (*.csv)|*.csv";
+            sfd.InitialDirectory = Application.StartupPath;
+            sfd.DefaultExt = "csv";
+            sfd.AddExtension = true;
+            if (sfd.ShowDialog() != DialogResult.OK) return;
+
+            using (StreamWriter sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
+            {
+                foreach (var u in users)
+                {
+                    sw.Write(u.ID);
+                    sw.Write(u.FullName);
+                    sw.WriteLine();
+                }
+            }
         }
     }
 }
