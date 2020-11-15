98e216d21c7aa4664e9fef7e455bc33acc4a6184
Áron Vikete <aron.vikete@stud.uni-corvinus.hu>
2020-11-15 17:32:40 +0100

    Absztrakció

---
+++ week8/Abstractions/Toy.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace week8.Abstractions
+{
+    public abstract class Toy : Label
+    {
+        public Toy()
+        {
+            AutoSize = false;
+            Width = 50;
+            Height = Width;
+            Paint += Toy_Paint;
+        }
+
+        private void Toy_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics);
+        }
+        protected void DrawImage(Graphics g)
+        {
+            g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
+        }
+        public virtual void MoveToy()
+        {
+            Left += 1;
+        }
+    }
+}
+
