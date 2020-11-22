c4b246c380e719b7598714e06ced2dab0061a064
Áron Vikete <aron.vikete@stud.uni-corvinus.hu>
2020-11-22 11:41:36 +0100

    Jelszó validálás teszt

--- UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -14,7 +14,7 @@ namespace UnitTestExample.Test
             TestCase("abcd1234", false),
             TestCase("irf@uni-corvinus", false),
             TestCase("irf.uni-corvinus.hu", false),
-            TestCase("irf@uni-corvinus.hu", true)
+            TestCase("irf@uni-corvinus.hu", true),
         ]
         public void TestValidateEmail(string email, bool expectedResult)
         {
@@ -25,6 +25,25 @@ namespace UnitTestExample.Test
             var actualResult = accountController.ValidateEmail(email);
 
             // Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test,
+            TestCase("abcdABCD", false),
+            TestCase("ABCD2345", false),
+            TestCase("abcd1234", false),
+            TestCase("aBc12", false),
+            TestCase("Abcd1234", true)
+            ]
+        public void TestValidatePassword(string password, bool expectedResult)
+        {
+            // Arrange
+            var accountController = new AccountController();
+
+            //Act
+            var actualResult = accountController.ValidatePassword(password);
+
+            //Assert
             Assert.AreEqual(expectedResult, actualResult);
         }
     }
